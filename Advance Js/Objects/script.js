0dca982e5307b354f4cec1e248f3d2df67044169
Emmanuel <twentekghana@gmail.com>
2020-05-27 00:00:23 +0800

    Filter

--- Advance Js/Objects/script.js
+++ Advance Js/Objects/script.js
@@ -25,6 +25,7 @@ var Person = function (name, yearOfBirth, job) {
 //Create an instance of object out of the constructor function
 
 var enkansah = new Person("Enkansah", 1988, "Programmer");
+
 //New = brand new object is created
 //The new object makes it posible to point this keyword not to the global but instead to the new object created
 // The new has all the properties we passed into the function constructor
@@ -70,5 +71,100 @@ Country.prototype.calcAge = function () {
   console.log("This is a method for calculating age");
 };
 
+///read
+// install npm install counrty
+
 var ghana = new Country("Ghana", 1957, "Nkrumah");
-console.log(ghana.calcAge());
+//console.log(ghana.calcAge());
+
+//Primitive and objects
+
+//Primitive holds their value in variable but object is not but it it's pass by reference thus it doesn't contain the object
+//Object variables does not hold their actual data but it points to the object in memory
+
+//strings
+//numbers
+//boolean
+
+//Primitive
+//All primitive variables holds their own value
+
+var a = 23;
+
+var b = a;
+
+b = 40;
+
+// console.log("a", a);
+// console.log("b", b);
+
+//Ojects
+//objects are passed by reference
+
+var obj3 = {
+  name: "John",
+  age: 40,
+};
+
+var obj4 = obj3;
+
+obj4.name = "Emma";
+// console.log("obj3", obj3);
+// console.log("obj4", obj4);
+
+//Passing objects to functions
+
+var age = 45;
+
+var person = {
+  name: "Ben",
+  city: " Kumasi",
+};
+
+//Passed in primitive value
+function change1(a) {
+  return a;
+}
+//calling the function
+var result = change1(age);
+//console.log(result);
+
+//Passed in object
+function change2(obj) {
+  return obj;
+}
+
+var result2 = change2(person);
+//console.log(result2);
+
+//=============
+//FIRST CLASS FUNCTIONS // HOC(High Order function)
+
+//A function is an instance of an object hence it behaves like an object
+
+//Store into a variable = function expression
+
+//Passing a function as an argument == callback == hoc
+
+//return a function from function
+
+//Funtion stored into a varibale
+var add = function (a, b) {
+  return a + b;
+};
+
+//Anonymous function: Is a function without a name
+var result = add(30, 10);
+//console.log(result);
+
+//passing a function as an argument
+function greeting() {
+  console.log("Hi how are you");
+}
+
+function welcome(fn) {
+  console.log("You are welcome");
+  console.log(fn);
+}
+
+console.log(welcome(greeting()));
